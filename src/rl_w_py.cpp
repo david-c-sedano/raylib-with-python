abc9d54daf423c676adb5606af24c44c58f751ec
BigBomboBlast <sedanodavid2005@gmail.com>
2025-05-14 10:40:30 -0500

    basic window example now works!!

--- src/rl_w_py.cpp
+++ src/rl_w_py.cpp
@@ -10,4 +10,25 @@ int add (int foo, int pippo)
 PYBIND11_MODULE(_rl_w_py, m) {
     m.doc() = "Make programming fun again!!";
     m.def("add_from_cpp", &add, "A test. This should add two numbers from REAL MACHINE CODE!!");
+
+    /* STRUCTS */
+	py::class_<Color>(m, "Color")
+		.def(py::init([](unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
+			return Color{r, g, b, a};
+		}))
+		.def_readwrite("r", &Color::r)
+		.def_readwrite("g", &Color::g)
+		.def_readwrite("b", &Color::b)
+		.def_readwrite("a", &Color::a);
+
+    /* RCORE FUNCTIONS */
+    m.def("InitWindow", &InitWindow, "InitWindow");
+    m.def("WindowShouldClose", &WindowShouldClose, "WindowShouldClose");
+    m.def("BeginDrawing", &BeginDrawing, "BeginDrawing");
+    m.def("EndDrawing", &EndDrawing, "EndDrawing");
+    m.def("ClearBackground", &ClearBackground, "ClearBackground");
+    m.def("CloseWindow", &CloseWindow, "CloseWindow");
+
+    /* RTEXT FUNCTIONS */
+    m.def("DrawText", &DrawText, "DrawText");
 }
